e02c1948fb8442d1133e5a140b7fffd455d0caed
gaddy <gaddyh@gmail.com>
2023-12-18 23:14:13 +0200

    as

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ if st.session_state.start_chat:
             run_id=run.id
             )
             #run_steps['data']['step_detail']['tool_calls']
-            st.write(run_steps.step_details)
+            st.write(run_steps['step_details'])
             time.sleep(1)
             run = client.beta.threads.runs.retrieve(
                 thread_id=st.session_state.thread_id,
